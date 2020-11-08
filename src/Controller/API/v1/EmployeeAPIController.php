ff653f29323e2f2f5d39436ad383f0afa00ea8a4
Mansur <vipgmi05@gmail.com>
2020-11-08 23:20:33 +0400

    fixes

--- src/Controller/API/v1/EmployeeAPIController.php
+++ src/Controller/API/v1/EmployeeAPIController.php
@@ -5,15 +5,10 @@ namespace App\Controller\API\v1;
 
 
 use App\Entity\Branch;
-use App\Entity\Employee;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonDecode;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -26,17 +21,19 @@ class EmployeeAPIController extends AbstractController
 {
 
     /**
-     * @Route("/getByBranch/{id}", methods={"GET"})
+     * @Route("/getAllFromBranch/{id}", methods={"GET"})
      * @param Branch $branch
      * @param SerializerInterface $serializer
      * @return Response
      */
-    public function getByBranch(Branch $branch, SerializerInterface $serializer): Response
+    public function getAllFromBranch(Branch $branch, SerializerInterface $serializer): Response
     {
         $employees = $branch->getEmployees()->toArray();
         foreach ($employees as $index => $employee) {
-            $employees[$index] =  $serializer->normalize($employee, null, [AbstractNormalizer::ATTRIBUTES => ['name', 'position']]);
+            $employees[$index] = $serializer->normalize($employee, null, [AbstractNormalizer::ATTRIBUTES => ['name', 'position']]);
         }
+
+        sort($employees);
 
         return $this->json(['status' => 'success', 'data' => $employees]);
     }
