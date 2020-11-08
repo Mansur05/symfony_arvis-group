0d23ae95270b7729cad7bc9b7f7f7ff5c07dc4d4
Mansur <vipgmi05@gmail.com>
2020-11-09 00:11:52 +0400

    ajax support for branches

---
+++ src/Controller/API/v1/BranchAPIController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller\API\v1;
+
+
+use App\Entity\Company;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+
+/**
+ * Class BranchAPIController
+ * @Route("/api/v1/branch")
+ * @package App\Controller\API\v1
+ */
+class BranchAPIController extends AbstractController
+{
+
+    /**
+     * @Route("/getAllFromCompany/{id}", methods={"GET"})
+     * @param Company $company
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    public function getAllFromCompany(Company $company, SerializerInterface $serializer): JsonResponse
+    {
+        $branches = $serializer->normalize(
+            $company->getBranches(), null,
+            [AbstractNormalizer::ATTRIBUTES => ['id', 'name', 'description'],]
+        );
+
+        return $this->json(['status' => 'success', 'data' => $branches]);
+    }
+
+}
--- src/Controller/API/v1/EmployeeAPIController.php
+++ src/Controller/API/v1/EmployeeAPIController.php
@@ -6,7 +6,7 @@ namespace App\Controller\API\v1;
 
 use App\Entity\Branch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -24,16 +24,14 @@ class EmployeeAPIController extends AbstractController
      * @Route("/getAllFromBranch/{id}", methods={"GET"})
      * @param Branch $branch
      * @param SerializerInterface $serializer
-     * @return Response
+     * @return JsonResponse
      */
-    public function getAllFromBranch(Branch $branch, SerializerInterface $serializer): Response
+    public function getAllFromBranch(Branch $branch, SerializerInterface $serializer): JsonResponse
     {
         $employees = $serializer->normalize(
-            $branch->getEmployees()->toArray(),
+            $branch->getEmployees(),
             null,
-            [
-                AbstractNormalizer::ATTRIBUTES => ['name', 'position']
-            ]
+            [AbstractNormalizer::ATTRIBUTES => ['name', 'position'],]
         );
 
         return $this->json(['status' => 'success', 'data' => $employees]);
