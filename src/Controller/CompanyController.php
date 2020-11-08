3b75d6e4d050aafdd047236eece2d56f54af36ce
Mansur <vipgmi05@gmail.com>
2020-11-08 10:56:43 +0400

    twig render

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    /**
+     * @Route("/company/{id}", name="index")
+     * @param Company $company
+     * @return Response
+     */
+    public function index(Company $company): Response
+    {
+        return $this->render('company/index.html.twig', [
+            'company' => $company,
+        ]);
+    }
+}
